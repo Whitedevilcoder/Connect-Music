668b0ff23e74f9ee55015afa4e05dbb3d0f64b01
G Praveen Kumar <gpraveenkumar6405@gmail.com>
2024-10-04 23:46:03 +0530

    add

--- Server/server.js
+++ Server/server.js
@@ -1,15 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
 
 app.get('/', (req, res)=>{
-    res.sendfile
-})
+   
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
