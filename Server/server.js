cb5a8488440f57e4b0511b0bd76baaa8a085f321
G Praveen Kumar <gpraveenkumar6405@gmail.com>
2024-10-04 23:54:19 +0530

    add

--- Server/server.js
+++ Server/server.js
@@ -6,13 +6,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
 
-app.get('/', (req, res)=>{
-   
-});
+
+app.use('/public', express.static(path.join(__dirname, '../client/public')));
+app.use('/src', express.static(path.join(__dirname, '../client/src')));
+
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// });
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
